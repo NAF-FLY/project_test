a73dc561e998071ddc57257f91ea02deb337c28e
Vadim <example@mail.com>
2022-07-06 13:30:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+
+// const answer = prompt("Вам есть 18?");
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
